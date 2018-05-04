1825e6f8ba8ec6db5871d6b576f5818eacd74e14
Aaron Meihm <ameihm@mozilla.com>
2018-05-04 11:53:28 -0500

    various cleanup to lambda function

--- systrack-lambda/main.go
+++ systrack-lambda/main.go
@@ -22,7 +22,8 @@ import (
 	"github.com/mozilla/scribe"
 )
 
-// pkgLogEnt describes the JSON structure we will recieve from the output source
+// pkgLogEnt describes the JSON structure we will recieve from the systrack command line
+// tool
 type pkgLogEnt struct {
 	Hostname  string          `json:"Hostname"`
 	Timestamp int64           `json:"Timestamp"`
@@ -31,11 +32,18 @@ type pkgLogEnt struct {
 }
 
 func (p *pkgLogEnt) validate() error {
+	if p.Hostname == "" {
+		return errors.New("package entry had no hostname")
+	}
 	return p.Fields.validate()
 }
 
+// toLogEntry converts a pkgLogEnt that has been identified as vulnerable into the line format
+// that will we push to firehose for storage in s3. v represents the vulnerability data applicable
+// to the package.
 func (p *pkgLogEnt) toLogEntry(v database.VulnerabilityWithAffected) string {
 	appname := "unknown"
+	// See if we can get the app name from the submitted instance tags
 	for _, x := range p.Fields.InstanceTags {
 		e := strings.Split(x, "=")
 		if len(e) != 2 {
@@ -72,6 +80,24 @@ func (p *pkgLogEntFields) validate() error {
 	if p.PkgVersion == "" {
 		return errors.New("package entry had no package version")
 	}
+	if p.PkgArch == "" {
+		return errors.New("package entry had no package arch")
+	}
+	if p.Dist == "" {
+		return errors.New("package entry had no distribution identifier")
+	}
+	if p.FQDN == "" {
+		p.FQDN = "unknown"
+	}
+	if p.AMI == "" {
+		p.AMI = "unknown"
+	}
+	if p.InstanceID == "" {
+		p.InstanceID = "unknown"
+	}
+	if p.InstanceType == "" {
+		p.InstanceType = "unknown"
+	}
 	return nil
 }
 
@@ -79,7 +105,7 @@ type config struct {
 	cacheDir     string // Cache directory for cache generation
 	inputSample  string // If set, read and process an input sample from path
 	makeCache    bool   // If true, cache will be generated
-	outputStream string // Kinesis output stream
+	outputStream string // Kinesis Firehose output stream
 
 	rhelData vulnsrc.UpdateResponse
 }
@@ -119,54 +145,61 @@ func kinesisWrite(dmap []string) error {
 	return nil
 }
 
-func checkVuln(p pkgLogEnt) error {
-	err := p.validate()
+func checkVuln(p pkgLogEnt) (ret []string, err error) {
+	err = p.validate()
 	if err != nil {
 		// Don't treat as fatal but log it
 		log.Printf("%v\n", err)
-		return nil
+		return ret, nil
 	}
-	log.Printf("validating for %v (%v)\n", p.Fields.PkgName, p.Fields.PkgVersion)
-	dmap := make([]string, 0)
-	// Check against RHEL/CentOS data
-	//
-	// XXX This is WIP right now, but should check the submitted distribution
-	// value
+	// Only centos:6 and centos:7 for now
+	if !strings.HasPrefix(p.Fields.Dist, "centos:") {
+		log.Printf("skipping unsupported dist %v for %v\n", p.Fields.Dist, p.Hostname)
+		return ret, nil
+	}
+	log.Printf("check %v on %v (%v)\n", p.Fields.PkgName, p.Hostname, p.Fields.PkgVersion)
 	for _, v := range cfg.rhelData.Vulnerabilities {
 		for _, w := range v.Affected {
+			if p.Fields.Dist != w.Namespace.Name {
+				continue
+			}
 			if w.FeatureName != p.Fields.PkgName {
 				continue
 			}
-			f, err := scribe.TestEvrCompare(scribe.EvropLessThan,
+			f, err := scribe.TestEvrCompare(scribe.EvropGreaterThan,
 				w.FixedInVersion, p.Fields.PkgVersion)
 			if err != nil {
-				return err
+				return ret, err
 			}
 			if f {
-				s := p.toLogEntry(v)
-				dmap = append(dmap, s)
+				ret = append(ret, p.toLogEntry(v))
 			}
 		}
 	}
-	if cfg.inputSample == "" && len(dmap) > 0 {
-		err = kinesisWrite(dmap)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	return ret, nil
 }
 
 func handler(ctx context.Context, kinesisEvent events.KinesisEvent) error {
 	log.Printf("handler executing for %v records\n", len(kinesisEvent.Records))
+	var obuf []string
 	for _, r := range kinesisEvent.Records {
 		var p pkgLogEnt
 		err := json.Unmarshal(r.Kinesis.Data, &p)
 		if err != nil {
+			// Don't treat this as fatal but log it
 			log.Printf("%v\n", err)
 			continue
 		}
-		err = checkVuln(p)
+		s, err := checkVuln(p)
+		if err != nil {
+			return err
+		}
+		if len(s) > 0 {
+			obuf = append(obuf, s...)
+		}
+	}
+	if len(obuf) > 0 {
+		err := kinesisWrite(obuf)
 		if err != nil {
 			return err
 		}
@@ -199,11 +232,14 @@ func main() {
 		log.Fatalf("%v\n", err)
 	}
 
-	// If in sample mode, just compare the sample data set against vulnerability
-	// data in the cache
 	if cfg.inputSample != "" {
-		var le pkgLogEnt
-		// Load a sample file, which should be mozlog entries with package
+		// If in sample mode, just compare the sample data set against vulnerability
+		// data in the cache
+		var (
+			le     pkgLogEnt
+			outbuf []string
+		)
+		// Load a sample file, which should be JSON mozlog entries with package
 		// information, one log line per entry
 		fd, err := os.Open(cfg.inputSample)
 		if err != nil {
@@ -217,13 +253,19 @@ func main() {
 			if err != nil {
 				log.Fatalf("%v\n", err)
 			}
-			err = checkVuln(le)
+			lns, err := checkVuln(le)
 			if err != nil {
 				log.Fatalf("%v\n", err)
+			}
+			if len(lns) > 0 {
+				outbuf = append(outbuf, lns...)
 			}
 		}
 		if scn.Err() != nil {
 			log.Fatalf("%v\n", scn.Err())
+		}
+		for _, x := range outbuf {
+			log.Printf("%v\n", x)
 		}
 	} else {
 		lambda.Start(handler)
